51062042a72740e120cc49df73e9b8c821328d5e
Lauren Richert <lsrichert@gmail.com>
2018-05-29 13:12:40 -0500

    adding functions

---
+++ main.js
@@ -0,0 +1,63 @@
+const Elizabeth = {
+    district: "5th"
+} 
+function changeElizabethDistrict (districtName){
+    Elizabeth.district = districtName
+}
+changeElizabethDistrict ("4th")
+console.log(Elizabeth.district)
+
+let platformStatements = {
+    taxes: "",
+    jobs: "",
+    infrastructure: "",
+    healthcare: "",
+    crime: ""
+}
+function changePlatformStatement (taxes){
+    platformStatements.taxes = taxes
+}
+changePlatformStatement ("we need lower taxes")
+console.log(platformStatements.taxes)
+
+
+let DonationForm = {
+    donationUrl: "",
+}
+
+let calendar = {
+    event1: "",
+    event2: "",
+    event3: "",
+}
+
+let volunteerInfo = {
+    name: "",
+    address: "",
+    email: "",
+    phone: "",
+    availability: "",
+    activities: "",
+}
+
+let biography = {
+    personal: "",
+    career: "",
+}
+
+let imageGallery = {
+    headShot: "",
+    pictureOfFamily: "",
+    pictureOfConstituents: "",
+}
+
+let missionStatement = {
+    line1: "",
+    line2: "",
+    line2: "",
+}
+
+let registerToVote = {
+    voteUrl: "", 
+}
+
